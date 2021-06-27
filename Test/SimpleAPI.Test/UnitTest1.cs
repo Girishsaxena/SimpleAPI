bde204e7d849bd44463a504a77cf58afed947f43
Girish SAXENA <Girish.SAXENA@ingenico.com>
2021-06-27 16:01:03 +0530

    Phase1

---
+++ Test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        // Arrange
+        WeatherForecastController we = new WeatherForecastController();
+        [Fact]
+        public void Test1()
+        {
+            //Act
+            var output = we.FetchData(1);
+            //assert
+            Assert.Equal("Girish Saxena",output);
+        }
+    }
+}
